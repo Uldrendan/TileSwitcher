a241c6a03b8157b43c266ee00828b5895b146232
Daniel Clarke <danclarke88@gmail.com>
2016-02-29 15:37:23 -0500

    Committing project info
    Tiles start magenta.  Click to turn cyan

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+
+	Rigidbody2D _rb;
+
+	void Start(){
+		_rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		float move = Input.GetAxis ("Horizontal");
+
+		_rb.velocity = new Vector2(move * maxSpeed, _rb.velocity.y);
+	}
+
+
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	SpriteRenderer _sprite;
+
+	void Start(){
+		_sprite = GetComponent<SpriteRenderer> ();
+	}
+
+	void OnMouseDown(){
+		_sprite.color = Color.cyan;
+	}
+}
